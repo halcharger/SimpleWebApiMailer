6f1c5b30755d3138ab8d784dfd7997b9acba5f96
halcharger <allen.firth@gmail.com>
2014-04-03 13:58:58 +0100

    changing controller name and small changes

---
+++ WebApiRole/SendController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net.Http;
+using System.Web.Http;
+using NExtensions;
+
+namespace WebApiRole
+{
+    public class SendController : ApiController
+    {
+        [HttpPost]
+        [Route("api/send")]
+        public IHttpActionResult SendEmail(Email email)
+        {
+            Console.WriteLine("Received email send request for: ".Append(email == null ? "<NULL>" : email.ToString()));
+            return Ok(new StringContent("boom!!!"));
+        }
+    }
+
+    public class Email
+    {
+        public string Subject { get; set; }
+        public string Body { get; set; }
+        public string From { get; set; }
+        public string To { get; set; }
+
+        public override string ToString()
+        {
+            return new[]{From, To, Subject}.JoinWithComma(StringJoinOptions.AddSpaceSuffix);
+        }
+    }
+}
