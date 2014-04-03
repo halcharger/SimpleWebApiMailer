4162a04b5c9195ccaa1e2c12e630841941427ef7
halcharger <allen.firth@gmail.com>
2014-04-03 13:01:39 +0100

    initial test project with test api endpoint

---
+++ WebApiRole/WorkerRole.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Diagnostics;
+using System.Net;
+using System.Threading;
+using Microsoft.WindowsAzure.ServiceRuntime;
+using Microsoft.Owin.Hosting;
+
+namespace WebApiRole
+{
+    public class WorkerRole : RoleEntryPoint
+    {
+        private IDisposable _app = null;
+
+        public override void Run()
+        {
+            // This is a sample worker implementation. Replace with your logic.
+            Trace.TraceInformation("WebApiRole entry point called", "Information");
+
+            while (true)
+            {
+                Thread.Sleep(10000);
+                Trace.TraceInformation("Working", "Information");
+            }
+        }
+
+        public override bool OnStart()
+        {
+            ServicePointManager.DefaultConnectionLimit = 12;
+
+            // New code:
+            var endpoint = RoleEnvironment.CurrentRoleInstance.InstanceEndpoints["MailerEndpoint"];
+            string baseUri = string.Format("{0}://{1}",
+                endpoint.Protocol, endpoint.IPEndpoint);
+
+            Trace.TraceInformation(String.Format("Starting OWIN at {0}", baseUri),
+                "Information");
+
+            _app = WebApp.Start<Startup>(new StartOptions(url: baseUri));
+
+            return base.OnStart();
+        }
+
+        public override void OnStop()
+        {
+            if (_app != null)
+            {
+                _app.Dispose();
+            }
+            base.OnStop();
+        }
+    }
+}
